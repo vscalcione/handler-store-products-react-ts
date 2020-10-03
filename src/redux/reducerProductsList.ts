9acff236f92b41d1725bbc4d6a0c1e35d9143a88
Vincenzo Scalcione <vincenzo99.scalcione@gmail.com>
2020-10-03 11:34:17 +0200

    Add redux files and some components

---
+++ src/redux/reducerProductsList.ts
@@ -0,0 +1,33 @@
+import { FETCH_PRODUCTS_LIST_SUCCESS, ADD_ITEM, REMOVE_ITEM } from "./actions";
+
+export const reducerProductsList = (prevState: any = {}, action: any) => {
+  let clonedState = JSON.parse(JSON.stringify(prevState));
+
+  switch (action.type) {
+    case "@@INIT":
+      clonedState.loading = true;
+      break;
+
+    case FETCH_PRODUCTS_LIST_SUCCESS:
+      clonedState.products = action.payload;
+      clonedState.loading = false;
+      break;
+
+    case ADD_ITEM:
+      clonedState.products.push(action.payload);
+      break;
+
+    case REMOVE_ITEM:
+      clonedState.products.map((product: any, index: number) => {
+        if (product.id === action.payload.id) {
+          clonedState.products.splice(index, 1);
+        }
+      });
+      break;
+
+    default:
+      break;
+  }
+
+  return clonedState;
+};
---
+++ src/redux/saga.ts
@@ -0,0 +1,27 @@
+import { put, call } from "redux-saga/effects";
+import {
+  FETCH_PRODUCTS_LIST_FAILURE,
+  FETCH_PRODUCTS_LIST_SUCCESS,
+} from "./actions";
+import { BASE_PATH } from "../config";
+
+const getListProducts = async () => {
+  const response = await fetch(`${BASE_PATH}`);
+  return response.json();
+};
+
+export function* getProductsList() {
+  try {
+    // ottengo tutta la lista dei prodotti tramite una chiamata asincrona
+    const payload = yield call(getListProducts);
+    yield put({
+      type: FETCH_PRODUCTS_LIST_SUCCESS,
+      payload,
+    });
+  } catch (e) {
+    yield put({
+      type: FETCH_PRODUCTS_LIST_FAILURE,
+      payload: { message: e.message },
+    });
+  }
+}
