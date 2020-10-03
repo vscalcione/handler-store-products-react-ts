3ad3f6b49abedad4dcefecad3f809e1dcdad64dd
Vincenzo Scalcione <vincenzo99.scalcione@gmail.com>
2020-10-03 17:09:57 +0200

    Fixing

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-const path = require('path');
-const express = require('express');
-
+const express = require("express");
+const favicon = require("express-favicon");
+const path = require("path");
+const port = process.env.PORT || 8080;
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.port || 3000;
 
-app.use(express.static(publicPath));
+app.use(favicon(__dirname + "/build/favicon.ico"));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get('*', (req, res) => {
-	res.sendFile(path.join(publicPath, 'index.html'));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(port, '0.0.0.0', () => {
-	console.log(`Server is up on http://localhost:${port}`);
-});
+app.listen(port);
